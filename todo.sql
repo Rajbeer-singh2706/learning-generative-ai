0d06263265b42c8f062fbdd46d15cd92834ab415
raj singh <rajsingh@rajs-MacBook-Air.local>
2024-10-30 11:19:18 +0530

    updated

---
+++ todo.sql
@@ -0,0 +1,10 @@
+## Need to work on this as well , slowly 
+
+## AWS GEN AI 
+## Databricks GEN AI 
+
+
+### Gen AI is must in today Software world , 
+
+if work in AWS => master with GEN AI 
+if work in databricks => Master with GEN AI 
